154de8bbe164545451e2e537c55e1c46c0974bcc
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-01-11 09:01:31 -0200

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+	<title>Hello, world!</title>
+</head>
+<body>
+<div class="container"> 
+	<h1>Hello, world!</h1>
+
+	<div class="container">
+
+        <div class="row">
+            <div class="col-12 col-sm-6 col-lg-8 border border-primary">.col-12 .col-sm-6 .col-lg-8</div>
+            <div class="col-6 col-lg-4 border border-primary">.col-6 .col-lg-4</div>
+        </div>
+
+        <div class="row">
+            <div class="col-6 col-sm-4 border border-primary">.col-6 .col-sm-4</div>
+            <div class="col-6 col-sm-4 border border-primary">.col-6 .col-sm-4</div>
+            <div class="col-6 col-sm-4 border border-primary">.col-6 .col-sm-4</div>
+        </div>
+
+
+        <!-- Empilhe as colunas, em dispositivos móveis, fazendo com que uma tenha largura total e a outra só metade -->
+        <div class="row">
+            <div class="col-12 col-md-8">.col-12 .col-md-8</div>
+            <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+        </div>
+
+        <div class="bd-example">
+
+            <span class="border border-primary"></span>
+            <span class="border border-secondary"></span>
+            <span class="border border-success"></span>
+            <span class="border border-danger"></span>
+            <span class="border border-warning"></span>
+            <span class="border border-info"></span>
+            <span class="border border-light"></span>
+            <span class="border border-dark"></span>
+            <span class="border border-white"></span>
+        </div>
+
+
+        <!-- Colunas começam com largura de 50%, em dispositivos móveis, e caem para 33.3% nos desktops -->
+        <div class="row">
+            <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+            <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+            <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+        </div>
+
+        <!-- Colunas sempre com metade da largura disponível, em dispositivos móveis e desktop -->
+        <div class="row">
+            <div class="col-6">.col-6</div>
+            <div class="col-6">.col-6</div>
+        </div>
+
+
+        <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+
+
+		<div class="row">
+			<div class="col-sm">
+				One of three columns
+			</div>
+			<div class="col-sm">
+				One of three columns
+			</div>
+			<div class="col-sm">
+				One of three columns
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-8">col-sm-8</div>
+			<div class="col-sm-4">col-sm-4</div>
+		</div>
+		<div class="row">
+			<div class="col-sm">col-sm</div>
+			<div class="col-sm">col-sm</div>
+			<div class="col-sm">col-sm</div>
+		</div>
+
+        <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+
+            <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+            <div class="row">
+                <div class="col-12 col-md-8 bg-primary text-white">.col-12 .col-md-8</div>
+                <div class="col-6 col-md-4 bg-success text-white">.col-6 .col-md-4</div>
+            </div>
+
+            <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+            <div class="row">
+                <div class="col-6 col-md-4 bg-secondary">.col-6 .col-md-4</div>
+                <div class="col-6 col-md-4 bg-danger">.col-6 .col-md-4</div>
+                <div class="col-6 col-md-4 bg-info">.col-6 .col-md-4</div>
+            </div>
+
+            <!-- Columns are always 50% wide, on mobile and desktop -->
+            <div class="row">
+                <div class="col-6 bg-warning">.col-6</div>
+                <div class="col-6 bg-primary">.col-6</div>
+            </div>
+
+        <div class="p-3 mb-2 bg-success text-white">.bg-primary</div>
+
+        <div class="row justify-content-start">
+            <div class="col-4">
+                One of two columns
+            </div>
+            <div class="col-4">
+                One of two columns
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-4">
+                One of two columns
+            </div>
+            <div class="col-4">
+                One of two columns
+            </div>
+        </div>
+        <div class="row justify-content-end">
+            <div class="col-4">
+                One of two columns
+            </div>
+            <div class="col-4">
+                One of two columns
+            </div>
+        </div>
+        <div class="row justify-content-around">
+            <div class="col-4">
+                One of two columns
+            </div>
+            <div class="col-4">
+                One of two columns
+            </div>
+        </div>
+        <div class="row justify-content-between">
+            <div class="col-4">
+                One of two columns
+            </div>
+            <div class="col-4">
+                One of two columns
+            </div>
+        </div>
+
+
+
+	</div>
+
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+</body>
+</html>
+
